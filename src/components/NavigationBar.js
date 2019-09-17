8a652e898fdea0b53f69459d5dac965dd8dc9c80
Dawit <dawit@ginetta.net>
2019-09-17 14:25:33 +0200

    Set up a directory called components and create a few components

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationBar = () => {
+	return (
+		<div className="navigation-bar" style={{ marginTop: '15px' }}>
+			To think of what to this could contain yet
+		</div>
+	);
+};
+
+export default NavigationBar;
