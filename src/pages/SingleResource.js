f405cec07bfc2121d7f879cab828db33bfdac845
Dawit <dawit@ginetta.net>
2019-09-17 14:00:56 +0200

    Populate the skeleton for the pages

---
+++ src/pages/SingleResource.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SingleResource = () => {
+	return (
+		<div>
+			<h2>Course Title</h2>
+			<h3>Course Overview</h3>
+			<p>Course Description</p>
+			<h6>Level</h6>
+		</div>
+	);
+};
+
+export default SingleResource;
---
+++ src/pages/resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resources = () => {
+	const data = {
+		title: 'React with Wes Bos',
+		description:
+			'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam ipsum accusantium incidunt animi quasi?',
+		level: 'beginners'
+	};
+	return <div />;
+};
+
+export default Resources;
