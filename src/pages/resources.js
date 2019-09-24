6d89c2ac5fa8f050cd339d52cb8ddfd6a904c37a
Dawit <dawit@ginetta.net>
2019-09-24 11:18:04 +0200

    Clean commented out props which were then replaced by the spread operator, rafactoring the code and making it more readable

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -9,12 +9,6 @@ const Resources = () => {
 			{allCoursesList.map((eachcourse) => (
 				<SingleResource
 					key={eachcourse.title}
-					/* title={eachcourse.title}
-					type={eachcourse.type}
-					overview={eachcourse.overview}
-					photo_link={eachcourse.photo_link}
-					level={eachcourse.level}
-					instructor_name={eachcourse.instructor_name} */
 					// the commented out here could be replaced using the spread operator below, to help make the lines of code conciser.
 					{...eachcourse}
 				/>
